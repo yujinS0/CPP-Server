abba25c1e85be988df938701e89307c26272e248
SYJ <SYJ@930YUJIN>
2024-11-11 20:04:12 +0900

    3단계 : 애플리케이션과 네트워크 코드의 분리 - 클라이언트 구조체에 인덱스 변수 추가

--- IOCP/IOCPServer/Define.h
+++ IOCP/IOCPServer/Define.h
@@ -3,7 +3,7 @@
 #include <winsock2.h>
 #include <Ws2tcpip.h>
 
-const UINT32 MAX_SOCKBUF = 256;	// 패킷 크기
+const UINT32 MAX_SOCKBUF = 256;		// 패킷 크기
 const UINT32 MAX_WORKERTHREAD = 4;  // 쓰레드 풀에 넣을 쓰레드 수
 
 
@@ -16,21 +16,22 @@ enum class IOOperation
 // WSAOVERLAPPED 구조체 확장
 struct stOverlappedEx
 {
-	WSAOVERLAPPED m_wsaOverlapped;		//Overlapped I/O구조체
-	SOCKET		m_socketClient;			//클라이언트 소켓
-	WSABUF		m_wsaBuf;				//Overlapped I/O작업 버퍼
-	IOOperation m_eOperation;			//작업 동작 종류
+	WSAOVERLAPPED m_wsaOverlapped;		// Overlapped I/O구조체
+	SOCKET m_socketClient;				// 클라이언트 소켓
+	WSABUF m_wsaBuf;					// Overlapped I/O작업 버퍼
+	IOOperation m_eOperation;			// 작업 동작 종류
 };
 
 // 클라이언트 정보를 담는 구조체
 struct stClientInfo
 {
-	SOCKET			m_socketClient;			// Cliet와 연결되는 소켓
-	stOverlappedEx	m_stRecvOverlappedEx;	// RECV Overlapped I/O작업을 위한 변수
-	stOverlappedEx	m_stSendOverlappedEx;	// SEND Overlapped I/O작업을 위한 변수
+	INT32 mIndex = 0;					    // 클라이언트 인덱스
+	SOCKET m_socketClient;					// Cliet와 연결되는 소켓
+	stOverlappedEx m_stRecvOverlappedEx;	// RECV Overlapped I/O작업을 위한 변수
+	stOverlappedEx m_stSendOverlappedEx;	// SEND Overlapped I/O작업을 위한 변수
 
-	char			mRecvBuf[MAX_SOCKBUF]; // 데이터 버퍼
-	char			mSendBuf[MAX_SOCKBUF]; // 데이터 버퍼
+	char mRecvBuf[MAX_SOCKBUF]; // 데이터 버퍼
+	char mSendBuf[MAX_SOCKBUF]; // 데이터 버퍼
 
 	stClientInfo()
 	{
